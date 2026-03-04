741cb291e7a79dc6d1c359cfa7706fa4c6fc8a81
Ozan Yarcı <ozanyarci@gmail.com>
2026-03-04 16:13:56 +0300

    feat: add google analytics tracking

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
-import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, effect } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ThemeService } from './services/theme.service';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -12,4 +15,17 @@ import { ThemeService } from './services/theme.service';
 export class App {
   protected readonly theme = inject(ThemeService);
   protected readonly title = signal('dev-lingo');
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if (typeof gtag === 'function') {
+        gtag('config', 'G-PC9R3WQCET', {
+          'page_path': event.urlAfterRedirects
+        });
+      }
+    });
+  }
 }
